ab24d41071b35b9c02c62ed6119c79e74dc7509a
davidlatuno <dslatuno@gmail.com>
2018-10-06 19:52:17 -0700

    Removed test scripts

--- davids_awesome_script.sh
+++ davids_awesome_script.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Lists user input
-# NAME=${1}
-# echo $NAME
-
-# Lists directory names in current directory (Basically ls)
-# for d in */ ; do
-#     echo "$d"
-# done
-
 # Check if solved folder exists
 for d in */ ; do
   if [ -d "$d/Solved" ]
