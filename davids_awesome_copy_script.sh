9067f8b4b3104ac95329175efc5d839f7e0b85d6
davidlatuno <dslatuno@gmail.com>
2018-10-07 02:44:22 -0700

    Add user input to restrict range of directory copy

--- davids_awesome_copy_script.sh
+++ davids_awesome_copy_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+START=$1
+END=$2
+INDEX=0
+declare -a arr
+for ((i=$START;i<=$END;i++)); do
+  arr+=($i)
+done
+
 # Get Basename
 PWD=`pwd`
 NAME=`basename $PWD`
@@ -9,8 +17,17 @@ cd 01-Activities
 
 # For All Activities get the solved (do logic to see if copying is necessary/possible) and copy it to gitlab folder
 for d in */ ; do
-  if [ -d "$d/Solved" -a -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d" -a ! -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d/Solved" ]
-  then
-    cp -r "$d/Solved" ../../../../UCSD201807FSF5/$NAME/01-Activities/$d
-  fi
+  # Iterate through the user defined a range of directories
+  for ((i=0;i<=${#arr[@]};i++)); do
+    # If this folder is in the range of user input
+    if [ "$((INDEX+1))" == "${arr[$i]}" ] ; then
+      # logic to see if copying is necessary/possible
+      if [ -d "$d/Solved" -a -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d" -a ! -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d/Solved" ]
+      then
+        cp -r "$d/Solved" ../../../../UCSD201807FSF5/$NAME/01-Activities/$d
+      fi
+    fi
+  done
+  # Increment directory index variable
+  INDEX=$((INDEX+1))
 done
