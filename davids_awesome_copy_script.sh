5c430061c88291660edcb1ceaadabe4692c98ea1
davidlatuno <dslatuno@gmail.com>
2018-10-06 22:38:59 -0700

    Refactor copy script to not need user input

--- davids_awesome_copy_script.sh
+++ davids_awesome_copy_script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Check if solved folder exists
+# Get Basename
+PWD=`pwd`
+NAME=`basename $PWD`
 
-FOLDER=${1}
+# Move into Activities Folder
+cd 01-Activities
 
+# For All Activities get the solved (do logic to see if copying is necessary/possible) and copy it to gitlab folder
 for d in */ ; do
-  if [ -d "$d/Solved" -a -d "../../../../UCSD201807FSF5/$FOLDER/01-Activities/$d" -a ! -d "../../../../UCSD201807FSF5/$FOLDER/01-Activities/$d/Solved" ]
+  if [ -d "$d/Solved" -a -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d" -a ! -d "../../../../UCSD201807FSF5/$NAME/01-Activities/$d/Solved" ]
   then
-    cp -r "$d/Solved" ../../../../UCSD201807FSF5/$FOLDER/01-Activities/$d
-    # echo "Directory $d'Solved' exists and $d exists in gitlab."
+    cp -r "$d/Solved" ../../../../UCSD201807FSF5/$NAME/01-Activities/$d
   fi
 done
